c6d807e265360f59ed62460db268f6086e569d1c
Lucas Nishida <57022139+Nishiders@users.noreply.github.com>
2019-11-12 18:03:59 -0300

    Update filapreferencial.c

--- filapreferencial.c
+++ filapreferencial.c
@@ -60,39 +60,78 @@ int consultarIdade(PFILA f, int id){
 
 
 bool inserirPessoaNaFila(PFILA f, int id, int idade){
-    if (idade < 0 || id < 0) return false;
-    int id_a = buscarID(f, id);
-    if (id = id_a) return false;
-    
-    PONT p = (PONT) malloc (sizeof(PONT));
-    p -> id = id;
-    p -> idade = idade;
+	    if (idade < 0 || id < 0) return false;
+    	PONT id_a = buscarID(f, id);
+    	if(id_a) return false;
+    	
+    	PONT p = (PONT) malloc (sizeof(ELEMENTO));
+    	p -> id = id;
+    	p -> idade = idade;
 
-    if(idade >= IDADEPREFERENCIAL){
-        if(f->inicio == NULL){
-            f->cabeca->ant = p;
-            f->cabeca->prox = p;
-            p->prox = f->cabeca;
-            p->ant = f->cabeca;
-        }
-        
-    }
+    	if(idade >= IDADEPREFERENCIAL){
+    	    if(f->cabeca->prox == f->cabeca){
+    	        f->cabeca->ant = p;
+    	        f->cabeca->prox = p;
+    	        p->prox = f->cabeca;
+    	        p->ant = f->cabeca;
+		return true;
+            }
+            if(f->cabeca->prox != f->cabeca && f->inicioNaoPref == f->cabeca){
+	    	p->prox = f->cabeca;
+	    	p->ant = f->cabeca->ant;
+	    	f->cabeca->ant->prox = p;
+	    	f-> cabeca->ant = p;
+	    	return true;
+	    }
+	    if(f->cabeca->prox != f->cabeca && f->inicioNaoPref != f->cabeca){
+	    	p->prox = f->inicioNaoPref;
+	    	p->ant = f->inicioNaoPref->ant;
+	    	f->inicioNaoPref->ant->prox = p;
+	    	f->inicioNaoPref->ant = p;
+	    	return true;
+	    }
+    	}
+    	if(idade < IDADEPREFERENCIAL){
+	    p->ant = f->cabeca->ant;
+	    p->prox = f->cabeca;
+	    f->cabeca->ant->prox = p;
+	    f->cabeca->ant = p;
+	    return true;
+     	}
 
 	return false;
 }
 
 bool atenderPrimeiraDaFila(PFILA f, int* id){
+	if(f->cabeca->prox == f->cabeca) return false;
+	
+	PONT apagar = f->cabeca->prox;
+	*id = apagar->id;
 
-
-
+	if(f->cabeca->prox != f->inicioNaoPref){
+	    f->cabeca->prox = f->cabeca->prox->prox;
+	    f->cabeca->prox->ant = f->cabeca;
+	    free(apagar);
+	    return true;
+	}
+	
+	else if(f->cabeca->prox == f->inicioNaoPref){
+  	    f->cabeca->prox = f->cabeca->prox->prox;
+	    f->cabeca->prox->ant = f->cabeca;
+	    f->inicioNaoPref = f->cabeca->prox;
+	    free(apagar);
+	    return true;
+	}
 
 	return false;
 }
 
 
 bool desistirDaFila(PFILA f, int id){
+    	if(f->cabeca->prox == f->cabeca) return false;
 
-
+	PONT desiste = buscarID(f, id);
+	if(!desiste) return false;
 
 	return false;
 }
